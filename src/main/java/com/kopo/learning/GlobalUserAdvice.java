8d860a3ca2174706d026e0440d723820cb0850cc
taehee <113709273+taeheehi@users.noreply.github.com>
2025-06-27 17:16:50 +0900

    feat(controller): ControllerAdvice로 user 모델 전역 주입 추가
    - 모든 뷰에서 user 정보를 공통 모델로 제공
    - 관리자 페이지에서 헤더 메뉴 유지 문제 해결

---
+++ src/main/java/com/kopo/learning/GlobalUserAdvice.java
@@ -0,0 +1,14 @@
+package com.kopo.learning;
+
+import com.kopo.learning.model.User;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@ControllerAdvice
+public class GlobalUserAdvice {
+    @ModelAttribute("user")
+    public User addUserToModel(HttpSession session) {
+        return (User) session.getAttribute("user");
+    }
+} 
