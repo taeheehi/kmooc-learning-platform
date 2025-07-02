f84680aa9d7f7efb64880ec7a628e476e9f5034c
taehee <113709273+taeheehi@users.noreply.github.com>
2025-07-02 15:46:07 +0900

    리팩토링: CSS 구조 및 템플릿 정리

--- src/main/java/com/kopo/learning/GlobalUserAdvice.java
+++ src/main/java/com/kopo/learning/GlobalUserAdvice.java
@@ -5,6 +5,8 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
+
+// 로그인한 사용자 정보를 전역적으로 뷰에 전달해주는 공통 설정 파일
 @ControllerAdvice
 public class GlobalUserAdvice {
     @ModelAttribute("user")
--- src/main/java/com/kopo/learning/LearningApplication.java
+++ src/main/java/com/kopo/learning/LearningApplication.java
@@ -3,6 +3,8 @@ package com.kopo.learning;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// 이 파일이 실제 서버를 구동시키는 진입점(Entry Point)입니다.
+// 서버를 실행할 때 가장 먼저 실행되는 파일
 @SpringBootApplication
 public class LearningApplication {
 
